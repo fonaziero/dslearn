d80b3f226bdd4c0ac39fb827e1b731717fdfdcc9
fonaziero <victorcarnevale@yahoo.com.br>
2022-10-27 00:14:28 -0300

    change to sts

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('BootCamp Html', 'https://eucontador.com.br/wp-content/uploads/2019/10/Cursos-Online.png', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-10-28T03:00:00.00Z', TIMESTAMP WITH TIME ZONE '2023-10-29T03:00:00.00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-11-28T03:00:00.00Z', TIMESTAMP WITH TIME ZONE '2023-12-29T03:00:00.00Z', 1);
